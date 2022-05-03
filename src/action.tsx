1fa09cad43ffc3ab21328cc9a8d842e04791614b
jonathanLindquist <jonathan.lindquist@protonmail.com>
2022-05-03 16:57:38 -0500

    adding actions

---
+++ src/action.tsx
@@ -0,0 +1,6 @@
+import { Dispatch } from "./interfaces";
+
+export const fetchDataAction = async (dispatch: Dispatch) => {
+  const URL =
+    "https://api.tvmaze.com/singlesearch/shows?q=la-casa-de-papel&embed=episodes";
+};
